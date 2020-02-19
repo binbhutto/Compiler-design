1f71fa5bef6883a2d817392550db61d65441a703
binbhutto <binbhuttoadil@gmail.com>
2020-02-19 08:04:16 +0530

    state table for lexical analyzer in added 2

--- 1_Assign/transition/main.c
+++ 1_Assign/transition/main.c
@@ -3,13 +3,8 @@
 #include 'stateTable.h'
 
 int main() {
-    std::map<char,int>Map;
-        Map['U']=0;
-        Map['C']=1;
-        Map['A']=2;
-        Map['G']=3;
+    printf("%d\n",stateTable[0][0]);
+    printf("%c\n", symbolIndexMap[0]);
     
-    printf("%d",Map['U']);
-
 
 }
